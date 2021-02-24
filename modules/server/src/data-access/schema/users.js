120e51970b2e3ee903c8ede978500c57edfbcb95
drbenschmidt <dr.ben.schmidt@gmail.com>
2021-02-24 10:58:46 -0600

    chore(docs): add er diagram (#3)

--- modules/server/src/data-access/schema/users.js
+++ modules/server/src/data-access/schema/users.js
@@ -3,12 +3,14 @@ import { sql } from '@databases/mysql';
 
 export const createUsersTable = async (db) => {
   await db.query(sql`
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL NOT NULL PRIMARY KEY,
-      email VARCHAR(128) NOT NULL,
-      password_hash VARCHAR(256) NOT NULL,
-      password_salt VARCHAR(256) NOT NULL,
-      UNIQUE(email)
-    )
+    CREATE TABLE 'users' (
+      'id' int(11) NOT NULL AUTO_INCREMENT,
+      'email' varchar(128) NOT NULL,
+      'password_hash' varchar(256) NOT NULL,
+      'password_salt' varchar(256) NOT NULL,
+      'isAdmin' tinyint(1) DEFAULT '0',
+      PRIMARY KEY ('id'),
+      UNIQUE KEY 'email_UNIQUE' ('email')
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
   `);
 };
