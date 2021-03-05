09aff7a6a47271c6a0cf1967f0dc0034c8967ee7
drbenschmidt <dr.ben.schmidt@gmail.com>
2021-03-04 19:27:16 -0600

    add server repositories

---
+++ modules/server/src/data-access/repositories/course-holes.ts
@@ -0,0 +1,79 @@
+/* eslint-disable class-methods-use-this */
+import { ConnectionPool, sql } from '@databases/mysql';
+import { Repository } from '../repository';
+
+export interface DbCourseHole {
+  id?: number;
+  number: number;
+  distance: number;
+  par: number;
+}
+
+class CourseHolesRepository implements Repository<DbCourseHole> {
+  db: ConnectionPool;
+
+  constructor(db: ConnectionPool) {
+    this.db = db;
+  }
+
+  async create(model: DbCourseHole): Promise<number> {
+    const [result] = await this.db.query(sql`
+      INSERT INTO courseHoles (number, distance, par)
+      VALUES (${model.number}, ${model.distance}, ${model.par});
+
+      SELECT LAST_INSERT_ID();
+    `);
+
+    // eslint-disable-next-line no-param-reassign
+    const id = result['LAST_INSERT_ID()'] as number;
+
+    return id;
+  }
+
+  async update(model: DbCourseHole): Promise<void> {
+    await this.db.query(sql`
+      UPDATE courseHoles
+      SET number=${model.number}, distance=${model.distance}, par=${model.par}
+      WHERE id=${model.id}
+    `);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.db.query(sql`
+      DELETE FROM courseHoles
+      WHERE id=${id}
+    `);
+  }
+
+  async get(id: number): Promise<DbCourseHole> {
+    const [entity] = await this.db.query(sql`
+      SELECT * FROM courseHoles
+      WHERE id=${id}
+    `);
+
+    if (entity) {
+      return entity;
+    }
+
+    return null;
+  }
+
+  async getAll(): Promise<DbCourseHole[]> {
+    const entities = await this.db.query(sql`
+      SELECT * FROM courseHoles
+    `);
+
+    return entities;
+  }
+
+  async getAllForCourseLayout(id: number): Promise<DbCourseHole[]> {
+    const entities = await this.db.query(sql`
+      SELECT * FROM courseHoles
+      WHERE courseLayoutId=${id}
+    `);
+
+    return entities;
+  }
+}
+
+export default CourseHolesRepository;
--- modules/server/src/data-access/repository-services.ts
+++ modules/server/src/data-access/repository-services.ts
@@ -2,6 +2,9 @@ import db from './database';
 import UsersRepository from './repositories/users';
 import ProfilesRepository from './repositories/players';
 import AliasesRepository from './repositories/aliases';
+import CoursesRepository from './repositories/courses';
+import CourseLayoutsRepository from './repositories/course-layouts';
+import CourseHolesRepository from './repositories/course-holes';
 
 class RepositoryServices {
   users: UsersRepository = null;
@@ -10,10 +13,19 @@ class RepositoryServices {
 
   aliases: AliasesRepository = null;
 
+  courses: CoursesRepository = null;
+
+  courseLayouts: CourseLayoutsRepository = null;
+
+  courseHoles: CourseHolesRepository = null;
+
   constructor() {
     this.users = new UsersRepository(db);
     this.profiles = new ProfilesRepository(db);
     this.aliases = new AliasesRepository(db);
+    this.courses = new CoursesRepository(db);
+    this.courseLayouts = new CourseLayoutsRepository(db);
+    this.courseHoles = new CourseHolesRepository(db);
   }
 }
 
