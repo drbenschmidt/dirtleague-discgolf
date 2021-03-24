538e1dc37ea644d3274bc547157f043cdf9a03bd
drbenschmidt <dr.ben.schmidt@gmail.com>
2021-03-24 18:58:57 -0500

    envvar for api root

--- modules/server/src/http/cors-handler.ts
+++ modules/server/src/http/cors-handler.ts
@@ -7,6 +7,10 @@ const corsHandler = cors({
     'http://127.0.0.1:3000',
     'http://localhost:8080',
     'http://localhost:8081',
+    'http://172.13.51.107',
+    'http://172.13.51.107:8080',
+    'http://dev.dirtleague.org',
+    'http://dev.dirtleague.org:8080',
   ],
   credentials: true,
 });
