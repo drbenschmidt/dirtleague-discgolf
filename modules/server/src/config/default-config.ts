0e06a245a23645c0c8fc22d0aa05252968e95b39
drbenschmidt <dr.ben.schmidt@gmail.com>
2021-03-27 13:53:50 -0500

    chore(oops): pushed up config for my test db server

--- modules/server/src/config/default-config.ts
+++ modules/server/src/config/default-config.ts
@@ -1,6 +1,6 @@
 const config = {
   DIRT_API_SESSION_SECRET: 'go pack go',
-  DIRT_API_SQL_STRING: 'mysql://ben:test@192.168.1.194:3306/test-db2',
+  DIRT_API_SQL_STRING: 'mysql://test-user:password@localhost:3306/test-db',
   DIRT_API_PORT: 8081,
 
   REACT_APP_API_ROOT: 'localhost:8081/api',
