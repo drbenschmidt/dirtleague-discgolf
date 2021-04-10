5a1ce49ba95a9fa8c66ea3cd8f0a483c71c71516
drbenschmidt <dr.ben.schmidt@gmail.com>
2021-04-09 20:59:20 -0500

    checkpoint: it at least works

--- modules/client/src/index.tsx
+++ modules/client/src/index.tsx
@@ -12,13 +12,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
